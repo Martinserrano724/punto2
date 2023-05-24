55c62d0e0a2f339f4c4dd714aa073b44fc7d539f
Martinserrano724 <martinserrano724@gmail.com>
2023-05-24 10:54:56 -0300

    Desarollo del punto 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Ejer2 from './components/Ejer2';
+
 
 function App() {
-
-
   return (
     <>
-      
+     <Ejer2 tituloExtra='My friend'/>
     </>
   )
 }
